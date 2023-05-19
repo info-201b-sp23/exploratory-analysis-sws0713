6aaeb6ecaaa1c784563b3e423334fb4aea565b2a
Mizzzean <129473207+Mizzzean@users.noreply.github.com>
2023-05-18 19:52:34 -0700

    Add files via upload

--- aggregate_table.R
+++ aggregate_table.R
@@ -1,24 +1,29 @@
 library(dplyr)
 library(knitr)
 
-setwd("~/Desktop/WorldCup")
-winners <- read.csv('WorldCups.csv', stringsAsFactors = FALSE)
-worldcup <- read.csv('WorldCupMatches.csv', stringsAsFactors = FALSE)
+winners <- read.csv('WorldCups.csv')
+
+worldcup <- read.csv('WorldCupMatches.csv')
 
 aggregate_table <- worldcup %>%
-  group_by(`Home.Team.Name`) %>%
+  group_by(Country = `Home.Team.Name`) %>%
   summarise(
     TotalMatches = n(),
     TotalWins = sum(`Home.Team.Goals` > `Away.Team.Goals`),
-    AverageGoalsScored = round(mean(`Home.Team.Goals`), 2),
-    AverageGoalsConceded = round(mean(`Away.Team.Goals`), 2),
+    AverageGoalsScored = mean(`Home.Team.Goals`),
+    AverageGoalsConceded = mean(`Away.Team.Goals`),
     Home.Team.Goals = sum(`Home.Team.Goals`),
-    Away.Team.Goals = sum(`Away.Team.Goals`),
-    Home.Team.Name = first(`Home.Team.Name`),
-    Away.Team.Name = first(`Away.Team.Name`)
+    Away.Team.Goals = sum(`Away.Team.Goals`)
   ) %>%
   arrange(desc(TotalWins))
 
-colnames(aggregate_table) <- c("Team", "Total Matches", "Total Wins", "Avg Goals Scored", "Avg Goals Conceded",
-                               "Total Home Goals", "Total Away Goals", "World Cup Wins")
+aggregate_table <- aggregate_table %>%
+  left_join(winners, by = c("Country" = "Country"))
+
+aggregate_table <- aggregate_table %>%
+  group_by(Country) %>%
+  mutate(WorldCupWins = sum(!is.na(Winner))) %>%
+  ungroup()
+
+aggregate_table <- aggregate_table %>% distinct(Country, .keep_all = TRUE)
 
