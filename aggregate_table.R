1da9c1aaf6873807d76792539dfe5253ff0bef6b
Mizzzean <129473207+Mizzzean@users.noreply.github.com>
2023-05-22 22:21:06 -0700

    Add files via upload

--- aggregate_table.R
+++ aggregate_table.R
@@ -1,30 +1,30 @@
 library(dplyr)
-library(knitr)
 
-
-winners <- read.csv('WorldCups.csv')
+setwd("~/Desktop/WorldCup")
 
 worldcup <- read.csv('WorldCupMatches.csv')
+winners <- read.csv('WorldCups.csv')
 
-aggregate_table <- worldcup %>%
-  group_by(Country = `Home.Team.Name`) %>%
+worldcup_grouped <- worldcup %>%
+  group_by(Home.Team.Name) %>%
   summarise(
-    TotalMatches = n(),
-    TotalWins = sum(`Home.Team.Goals` > `Away.Team.Goals`),
-    AverageGoalsScored = mean(`Home.Team.Goals`),
-    AverageGoalsConceded = mean(`Away.Team.Goals`),
-    Home.Team.Goals = sum(`Home.Team.Goals`),
-    Away.Team.Goals = sum(`Away.Team.Goals`)
-  ) %>%
-  arrange(desc(TotalWins))
+    Total_Matches = n(),
+    Total_Goals_Scored = sum(Home.Team.Goals + Away.Team.Goals),
+    Total_Home_Goals = sum(Home.Team.Goals),
+    Total_Away_Goals = sum(Away.Team.Goals),
+    Average_Attendance = round(mean(Attendance))
+  )
 
-aggregate_table <- aggregate_table %>%
-  left_join(winners, by = c("Country" = "Country"))
+winners_grouped <- winners %>%
+  group_by(Runners.Up) %>%
+  summarise(
+    Total_Wins = n(),
+    Total_Goals_Scored = sum(GoalsScored),
+    Average_Attendance = round(mean(Attendance))
+  )
 
-aggregate_table <- aggregate_table %>%
-  group_by(Country) %>%
-  mutate(WorldCupWins = sum(!is.na(Winner))) %>%
-  ungroup()
+colnames(worldcup_grouped) <- c("Team", "Total Matches", "Total Goals Scored", "Total Home Goals", "Total Away Goals", "Average Attendance")
 
-aggregate_table <- aggregate_table %>% distinct(Country, .keep_all = TRUE)
+colnames(winners_grouped) <- c("Team", "Total Wins", "Total Goals Scored", "Total Home Goals", "Total Away Goals", "Average Attendance")
+
 
