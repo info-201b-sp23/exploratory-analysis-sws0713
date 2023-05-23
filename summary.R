60abf594feeddcf1c1cd5c97fd5b0f9ffe4f4869
Taran Bains <taranbains1023@gmail.com>
2023-05-22 22:46:04 -0700

    Deleted unnecessary files

--- summary.R
+++ summary.R
@@ -2,32 +2,31 @@ library("dplyr")
 library("stringr")
 
 # Store the dataframes
-wc_df <- read.csv("~/Desktop/Info-201/exploratory-analysis-sws0713/WorldCups.csv")
-matches_df <- read.csv("~/Desktop/Info-201/exploratory-analysis-sws0713/WorldCupMatches.csv")
+wc_df <- read.csv("WorldCups.csv")
+matches_df <- read.csv("WorldCupMatches.csv")
 
-View(matches_df)
-# What are the top 5 countries with the most World Cups won in order?
+# What is the country with the most World Cups won?
 winners_table <- wc_df %>%
   group_by(Winner) %>%
   summarize(number_of_cups = n()) %>%
   arrange(desc(number_of_cups)) %>%
-  head(5)
+  head(1)
 
-# What are the top 5 countries that have hosted the World Cup the most?
+# What is the country that has hosted the World Cup the most?
 hosts_table <- wc_df %>%
   group_by(Country) %>%
   summarize(number_hosted = n()) %>%
   arrange(desc(number_hosted)) %>%
-  head(5)
+  head(1)
 
-# What are the top 5 host countries with the highest attendance?
+# What is the host country with the highest attendance?
 attendance_table <- wc_df %>%
   select(Year, Country, Attendance) %>%
   mutate(Attendance = as.numeric(gsub("\\.", "", Attendance))) %>%
   arrange(desc(Attendance)) %>%
   head(5)
 
-# What are the top 5 countries with the highest number of goals scored?
+# What is the country with the highest number of goals scored?
 goal_summary <- matches_df %>%
   group_by(Home.Team.Name) %>%
   summarize(total_goals = sum(Home.Team.Goals)) %>%
@@ -39,4 +38,4 @@ goal_summary <- matches_df %>%
   group_by(country) %>%
   summarize(total_goals = sum(total_goals)) %>%
   arrange(desc(total_goals)) %>%
-  head(5)
+  head(1)
